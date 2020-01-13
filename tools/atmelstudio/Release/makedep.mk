1ef38d8ec880835d8d8ef40dbe33945b3c52aeda
Rogier Reerink <me@rogier.io>
2020-01-13 10:16:19 +0100

    Library update.

---
+++ tools/atmelstudio/Release/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+..\..\src\can.c
+
+..\..\src\logger.c
+
+..\..\src\main.c
+
+..\..\src\nodeman.c
+
+..\..\src\spi.c
+
+..\..\tests\can_rx.c
+
+..\..\tests\can_tx.c
+
